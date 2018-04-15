a5c62092d8f13a6042a16fe19caccce1a9bc90a9
Hervé Ah-Leung <herve.ahleung@gmail.com>
2018-04-15 10:18:52 +0100

    Implement a git command

--- README.md
+++ README.md
@@ -34,6 +34,7 @@ boom boom! No way it's working!
 - [Docker Go starter](https://hub.docker.com/_/golang/)
 - [Dockerfile best practices](https://docs.docker.com/v17.09/engine/userguide/eng-image/dockerfile_best-practices/#use-multi-stage-builds)
 - [Golang dep](https://gist.github.com/subfuzion/12342599e26f5094e4e2d08e9d4ad50d)
+- [Golang Command Line Runner](https://golang.org/pkg/os/exec/)
 
 ## Cheat Sheet
 
--- bolt.go
+++ bolt.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "log"
   "os"
+  "os/exec"
+  "strings"
 
   "github.com/urfave/cli"
 )
@@ -12,11 +14,30 @@ func main() {
   app := cli.NewApp()
   app.EnableBashCompletion = true
 
-  app.Name = "boom"
-  app.Usage = "make an explosive entrance"
+  app.Name = "Bolt"
+  app.Usage = "Bolt is wrapping various helpers"
   app.Action = func(c *cli.Context) error {
-    fmt.Println("boom boom! No way it's working!")
+    fmt.Println("This is Bolt, try `bolt help` for more")
     return nil
+  }
+
+  app.Commands = []cli.Command{
+    {
+      Name:    "logs",
+      Aliases: []string{"l"},
+      Usage:   "See the current git logs of your folder",
+      Action:  func(c *cli.Context) error {
+        commandLine := strings.Split("git log --graph --abbrev-commit --decorate", " ")
+        out, err := exec.Command(commandLine[0], commandLine[1:]...).Output()
+
+        if err != nil {
+          log.Fatal(err)
+        }
+
+        fmt.Printf("%s", out)
+        return nil
+      },
+    },
   }
 
   err := app.Run(os.Args)
