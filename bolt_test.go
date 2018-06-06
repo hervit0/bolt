cd55bbd0babbba1dbccad34d5bd93c7433331261
Daniel Watts <danbadge@gmail.com>
2018-06-06 17:59:14 +0100

    Fix the failing test

--- bolt_test.go
+++ bolt_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestExample(t *testing.T) {
 	actual := 1
-	expected := 2
+	expected := 1
     if actual != expected {
        t.Errorf("Failed, got: %d, want: %d.", actual, expected)
     }
